a1f4398429bf092f1e85034a8b67560d245af2d9
kingjeongkong <wjdqls9223@gmail.com>
2025-02-04 01:43:57 +0900

    [feat] 메뉴 주문 list에 주문 취소 추가

--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,17 @@ public class Main {
         while(true) {
             try {
                 System.out.println("\n 🔹 1단계: 햄버거를 선택하세요.");
+                System.out.println("0)주문 취소");
                 for (int i = 0; i < Burger.BURGERS.size(); i++) {
                     Burger burger = Burger.BURGERS.get(i);
                     System.out.println((i + 1) + ")" + burger.getName() + " : " + burger.getPrice());
                 }
                 System.out.print("선택: ");
                 burgerChoice = scanner.nextInt();
+                if (burgerChoice == 0) {
+                    System.out.println("\n주문이 취소되었습니다.");
+                    return;
+                }
                 if (burgerChoice >= 1 && burgerChoice <= Burger.BURGERS.size()) {
                     break;
                 }
@@ -54,12 +59,17 @@ public class Main {
             while (true) {
                 try {
                     System.out.println("\n 🔹 2단계: 사이드를 선택하세요.");
+                    System.out.println("0)주문 취소");
                     for (int i = 0; i < Side.SIDES.size(); i++) {
                         Side side = Side.SIDES.get(i);
                         System.out.println((i + 1) + ")" + side.getName() + " : " + side.getPrice());
                     }
                     System.out.print("선택: ");
                     sideChoice = scanner.nextInt();
+                    if (sideChoice == 0) {
+                        System.out.println("\n주문이 취소되었습니다.");
+                        return;
+                    }
                     if (sideChoice >= 1 && sideChoice <= Side.SIDES.size()) {
                         break;
                     }
@@ -87,11 +97,16 @@ public class Main {
                 while (true) {
                     try {
                         System.out.println("\n어떤 추가 사이드 선택을 하시겠습니까?");
+                        System.out.println("0)주문 취소");
                         for (int i = 0; i < AdditionalSide.CATEGORIES.size(); i++) {
                             System.out.println((i + 1) + ") " + AdditionalSide.CATEGORIES.get(i));
                         }
                         System.out.print("선택: ");
                         additionalSideCategoryChoice = scanner.nextInt();
+                        if (additionalSideCategoryChoice == 0) {
+                            System.out.println("\n주문이 취소되었습니다.");
+                            return;
+                        }
                         if (additionalSideCategoryChoice >= 1 && additionalSideCategoryChoice <= AdditionalSide.CATEGORIES.size()) {
                             break;
                         }
@@ -110,11 +125,16 @@ public class Main {
                 while (true) {
                     try {
                         System.out.println("\n 🔹 " + selectedCategory + " 메뉴:");
+                        System.out.println("0)주문 취소");
                         for (int i = 0; i < additionalSides.size(); i++) {
                             System.out.println((i + 1) + ") " + additionalSides.get(i).getName() + " : " + additionalSides.get(i).getPrice());
                         }
                         System.out.print("선택: ");
                         additionalSideChoice = scanner.nextInt();
+                        if (additionalSideChoice == 0) {
+                            System.out.println("\n주문이 취소되었습니다.");
+                            return;
+                        }
                         if (additionalSideChoice >= 1 && additionalSideChoice <= additionalSides.size()) {
                             break;
                         }
@@ -132,12 +152,17 @@ public class Main {
             while (true) {
                 try {
                     System.out.println("\n 🔹 3단계: 음료를 선택하세요.");
+                    System.out.println("0)주문 취소");
                     for (int i = 0; i < Drink.DRINKS.size(); i++) {
                         Drink drink = Drink.DRINKS.get(i);
                         System.out.println((i + 1) + ")" + drink.getName() + " : " + drink.getPrice());
                     }
                     System.out.print("선택: ");
                     drinkChoice = scanner.nextInt();
+                    if (drinkChoice == 0) {
+                        System.out.println("\n주문이 취소되었습니다.");
+                        return;
+                    }
                     if (drinkChoice >= 1 && drinkChoice <= Drink.DRINKS.size()) {
                         break;
                     }
